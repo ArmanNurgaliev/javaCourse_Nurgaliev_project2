0cc2ec2b50482bdff821e517c2d2cdf9482073ce
Arman Nurgaliev <armanurgaliev@gmail.com>
2021-12-04 20:11:07 +0700

    Calc1

--- javaCourse_Nurgaliev_Ex4/src/Calc.java
+++ javaCourse_Nurgaliev_Ex4/src/Calc.java
@@ -1,2 +1,13 @@
+import java.util.Scanner;
+
 public class Calc {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("1. Ввести пример");
+        System.out.println("2. Продолжить работать с предыдущим ответом");
+        System.out.println("3. Выход");
+
+
+    }
 }
