bf9262c36e127f180eae2afe54b7a3331afec8e2
Arman Nurgaliev <armanurgaliev@gmail.com>
2022-01-22 21:15:31 +0700

    gsdfg

---
+++ javaCourse_Nurgaliev_Ex11/src/Lesson.java
@@ -0,0 +1,9 @@
+public enum Lesson {
+    Math("Math"), Russian("Russian"), Physics("Physics"), Biology("Biology");
+
+    private String name;
+
+    Lesson(String name) {
+        this.name = name;
+    }
+}
