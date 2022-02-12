8c19db00ed6851fbfc6f96993001f1768b163dc9
Arman Nurgaliev <49062511+ArmanNurgaliev@users.noreply.github.com>
2022-02-12 16:04:31 +0700

    Delete javaCourse_Nurgaliev_Ex11 directory

--- javaCourse_Nurgaliev_Ex11/src/Lesson.java
+++
@@ -1,9 +0,0 @@
-public enum Lesson {
-    Math("Math"), Russian("Russian"), Physics("Physics"), Biology("Biology");
-
-    private String name;
-
-    Lesson(String name) {
-        this.name = name;
-    }
-}
