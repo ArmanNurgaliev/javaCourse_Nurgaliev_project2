2267d3b7f705932bae841f2e346f479f80dc6745
Arman Nurgaliev <49062511+ArmanNurgaliev@users.noreply.github.com>
2022-02-12 16:05:03 +0700

    Delete javaCourse_Nurgaliev_Ex8 directory

--- javaCourse_Nurgaliev_Ex8/src/person/ClickHandler.java
+++
@@ -1,7 +0,0 @@
-package person;
-
-public interface ClickHandler {
-    default void printColor(Season season) {
-        System.out.println("Сейчас " + season.name() + ". " + season.getColorName());
-    }
-}
--- javaCourse_Nurgaliev_Ex8/src/person/Printable.java
+++
@@ -1,7 +0,0 @@
-package person;
-
-public interface Printable {
-    default void printFields(String name, int age, String bankName) {
-        System.out.printf("Name: %s, age %d, bank %s\n", name, age, bankName);
-    }
-}
